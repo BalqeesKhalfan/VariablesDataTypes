a6006afce41d846ab555358a5d2dfd400583d433
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-05 17:58:53 +0400

    Create Calculate for accepting veriables with  Integer Data Type  in java

---
+++ IntegerCalculator.java
@@ -0,0 +1,18 @@
+class IntegerCalculator {
+    public static void main(String[] args) {
+        int firstNum=12;
+        int seconNum=8;
+        
+        int addition = firstNum + seconNum;
+         int subtraction = firstNum - seconNum;
+         int div= firstNum / seconNum;
+         int mul = firstNum * seconNum;
+        System.out.println("Addition is " + addition);
+         System.out.println("subtraction is " + subtraction);
+          System.out.println("Divition is " + div);
+         System.out.println("Multiplication is " + mul);
+        
+        
+        
+    }
+}
